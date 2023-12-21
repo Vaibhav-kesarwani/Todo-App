7463f788e866ed79c75bbe66c288f5381f843c19
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-21 22:18:36 +0530

    added widthspace and the height spacer

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todoapp/common/utils/constants.dart';
 import 'package:todoapp/common/widgets/appstyle.dart';
 import 'package:todoapp/common/widgets/reusable_text.dart';
+import 'package:todoapp/common/widgets/width_spacer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,9 +16,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ResuableText(
-          text: "Todo App",
-          style: appstyle(26, AppConst.kBlueLight, FontWeight.bold),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ResuableText(
+              text: "Todo App",
+              style: appstyle(26, AppConst.kBlueLight, FontWeight.bold),
+            ),
+            const WidthSpacer(width: 20),
+            ResuableText(
+              text: "Todo App",
+              style: appstyle(26, AppConst.kBlueLight, FontWeight.bold),
+            ),
+            const WidthSpacer(width: 20),
+            ResuableText(
+              text: "Todo App",
+              style: appstyle(26, AppConst.kBlueLight, FontWeight.bold),
+            ),
+            const WidthSpacer(width: 20),
+          ],
         ),
       ),
     );

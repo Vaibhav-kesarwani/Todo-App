28ceb13200647dba8b373a632a1487a3268e1eb3
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-22 21:09:43 +0530

    page one completed

--- lib/features/onboarding/widgets/page_one.dart
+++ lib/features/onboarding/widgets/page_one.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todoapp/common/utils/constants.dart';
+import 'package:todoapp/common/widgets/appstyle.dart';
+import 'package:todoapp/common/widgets/height_spacer.dart';
+import 'package:todoapp/common/widgets/reusable_text.dart';
 
 class PageOne extends StatelessWidget {
   const PageOne({super.key});
@@ -10,6 +14,39 @@ class PageOne extends StatelessWidget {
       height: AppConst.kHeight,
       width: AppConst.kWidth,
       color: AppConst.kBkDark,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w),
+            child: Image.asset("assets/todo.png"),
+          ),
+          const HeightSpacer(height: 100),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ResuableText(
+                text: "ToDo App",
+                style: appstyle(
+                  30,
+                  AppConst.kLight,
+                  FontWeight.w600,
+                ),
+              ),
+              const HeightSpacer(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Welcome!! Do yo want to create a task fast and with ease",
+                  textAlign: TextAlign.center,
+                  style: appstyle(16, AppConst.kGreyLight, FontWeight.normal),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/onboarding/widgets/page_two.dart
+++ lib/features/onboarding/widgets/page_two.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todoapp/common/utils/constants.dart';
+import 'package:todoapp/common/widgets/height_spacer.dart';
 
 class PageTwo extends StatelessWidget {
   const PageTwo({super.key});
@@ -9,7 +11,18 @@ class PageTwo extends StatelessWidget {
     return Container(
       height: AppConst.kHeight,
       width: AppConst.kWidth,
-      color: AppConst.kBlueLight,
+      color: AppConst.kBkDark,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w),
+            child: Image.asset("assets/todo.png"),
+          ),
+          const HeightSpacer(height: 50),
+        ],
+      ),
     );
   }
 }

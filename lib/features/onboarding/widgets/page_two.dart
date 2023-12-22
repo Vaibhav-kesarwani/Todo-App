90500719b554c19443f085e7eb3b232c4f90efa1
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-22 22:23:54 +0530

    completed the onboarding screen

--- lib/features/onboarding/widgets/page_two.dart
+++ lib/features/onboarding/widgets/page_two.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todoapp/common/utils/constants.dart';
+import 'package:todoapp/common/widgets/custom_outlinebtn.dart';
 import 'package:todoapp/common/widgets/height_spacer.dart';
+import 'package:todoapp/features/auth/pages/login_page.dart';
 
 class PageTwo extends StatelessWidget {
   const PageTwo({super.key});
@@ -21,6 +23,21 @@ class PageTwo extends StatelessWidget {
             child: Image.asset("assets/todo.png"),
           ),
           const HeightSpacer(height: 50),
+          CustomOutlineButton(
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+              );
+            },
+            width: AppConst.kWidth * 0.9,
+            height: AppConst.kHeight * 0.06,
+            color2: Colors.transparent,
+            color: AppConst.kLight,
+            text: "Login with a phone number",
+          ),
         ],
       ),
     );

d8305728f920812e24b954d73c743d52aefdc71e
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-24 13:18:14 +0530

    added pinput for the phone auth

--- lib/features/auth/pages/otp_page.dart
+++ lib/features/auth/pages/otp_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pinput/pinput.dart';
+import 'package:todoapp/common/utils/constants.dart';
+import 'package:todoapp/common/widgets/appstyle.dart';
+import 'package:todoapp/common/widgets/height_spacer.dart';
+import 'package:todoapp/common/widgets/reusable_text.dart';
 
 class OtpPage extends StatelessWidget {
   const OtpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              HeightSpacer(height: AppConst.kHeight * 0.12),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.w),
+                child: Image.asset(
+                  "assets/todo.png",
+                  width: AppConst.kWidth * 0.5,
+                ),
+              ),
+              const HeightSpacer(height: 26),
+              ResuableText(
+                text: "Enter your otp",
+                style: appstyle(18, AppConst.kLight, FontWeight.bold),
+              ),
+              const HeightSpacer(height: 26),
+              Pinput(
+                length: 6,
+                showCursor: false,
+                onCompleted: (value) {
+                  if (value.length == 6) {}
+                },
+                onSubmitted: (value) {
+                  if (value.length == 6) {}
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

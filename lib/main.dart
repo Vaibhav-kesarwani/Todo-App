96b2109e5b0d917caddd4b754741f016e1abdeb1
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-28 14:00:59 +0530

    added the android dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,6 +16,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final defaultLightColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+  );
+
+  static final defaultDarkColorScheme = ColorScheme.fromSwatch(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blue,
+  );
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -22,16 +32,25 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 825),
       minTextAdapt: true,
       builder: (context, child) {
-        return MaterialApp(
-          title: "Todo App",
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            scaffoldBackgroundColor: AppConst.kBkDark,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          ),
-          themeMode: ThemeMode.dark,
-          home: const HomePage(),
-        );
+        return DynamicColorBuilder(
+            builder: (lightColorScheme, darkColorScheme) {
+          return MaterialApp(
+            title: "Todo App",
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              scaffoldBackgroundColor: AppConst.kBkDark,
+              colorScheme: lightColorScheme ?? defaultLightColorScheme,
+              useMaterial3: true,
+            ),
+            darkTheme: ThemeData(
+              scaffoldBackgroundColor: AppConst.kBkDark,
+              colorScheme: darkColorScheme ?? defaultDarkColorScheme,
+              useMaterial3: true,
+            ),
+            themeMode: ThemeMode.dark,
+            home: const HomePage(),
+          );
+        });
       },
     );
   }

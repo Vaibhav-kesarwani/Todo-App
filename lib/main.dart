0f95181da4ce9633492f062fb98325f57002dc36
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-12-28 16:12:15 +0530

    updated to do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,23 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:todoapp/common/utils/constants.dart';
-import 'package:todoapp/features/todo/pages/homepage.dart';
+import 'package:flutter/services.dart';
+import './screens/home.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  static final defaultLightColorScheme = ColorScheme.fromSwatch(
-    primarySwatch: Colors.blue,
-  );
-
-  static final defaultDarkColorScheme = ColorScheme.fromSwatch(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.blue,
-  );
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      useInheritedMediaQuery: true,
-      designSize: const Size(375, 825),
-      minTextAdapt: true,
-      builder: (context, child) {
-        return DynamicColorBuilder(
-            builder: (lightColorScheme, darkColorScheme) {
-          return MaterialApp(
-            title: "Todo App",
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              scaffoldBackgroundColor: AppConst.kBkDark,
-              colorScheme: lightColorScheme ?? defaultLightColorScheme,
-              useMaterial3: true,
-            ),
-            darkTheme: ThemeData(
-              scaffoldBackgroundColor: AppConst.kBkDark,
-              colorScheme: darkColorScheme ?? defaultDarkColorScheme,
-              useMaterial3: true,
-            ),
-            themeMode: ThemeMode.dark,
-            home: const HomePage(),
-          );
-        });
-      },
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo App',
+      home: Home(),
     );
   }
 }
